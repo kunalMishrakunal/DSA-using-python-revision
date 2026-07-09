934648ebe208d974bfdeed428ad69b3374fd277d
Kunal Mishra <knlkunal242@gmail.com>
2026-07-09 11:19:04 +0000

    its list using python

---
+++ list/list.py
@@ -0,0 +1,17 @@
+#List = list is a buield in data structure use to store different tipe of element in a single variable name and data can be any type.
+# list are creat using square [] brackets.
+
+#list with values:-
+l_1 = [1, 2, 3, 4]
+
+print("This is list with values: ", l_1)
+
+#Empty list:-
+l_2 = []
+
+print("This list is empty: ", l_2)
+
+#list with mixed values
+l_3 = [1, 'Radha', 'Krishna', 2]
+
+print("This list with mixed values: ", l_3)
